da03410b3b3b716708765c24a6878309f417599f
Davy Glerum <dglerum@Davys-MacBook-Pro.local>
2018-09-25 19:06:27 +0200

    removed comments

--- bin/update_new_commits_sensor.sh
+++ bin/update_new_commits_sensor.sh
@@ -2,7 +2,10 @@
 cd "/data/config"
 git fetch
 commits="$(git rev-list --count master..origin/master)"
+dev_commits="$(git rev-list --count dev..origin/dev)"
+
 branch="$(git branch | grep \* | cut -d ' ' -f2)"
 
 curl -X POST -H "x-ha-access: $1" -H "Content-Type: application/json" http://127.0.0.1:8123/api/states/sensor.new_commits -d "{\"state\": \"$commits\"}"
+curl -X POST -H "x-ha-access: $1" -H "Content-Type: application/json" http://127.0.0.1:8123/api/states/sensor.new_dev_commits -d "{\"state\": \"$dev_commits\"}"
 curl -X POST -H "x-ha-access: $1" -H "Content-Type: application/json" http://127.0.0.1:8123/api/states/sensor.current_branch -d "{\"state\": \"$branch\"}"
